996aa501fdf7ed0160ab7f4f448da22db8cef434
johnpauljpc <jpcwork081@gmail.com>
2025-07-23 15:05:04 +0100

    product and category models created

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'unit_price', 'total_price']
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length = 100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    available_quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(decimal_places=2, default=0.00, max_digits=10)
+    total_price = models.DecimalField(decimal_places=2, max_digits=20,editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.unit_price * self.available_quantity
+        return super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.name
