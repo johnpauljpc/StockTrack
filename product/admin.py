a4bc2df26a5e2c454d0c30bfa22e33e089b79d49
johnpauljpc <jpcwork081@gmail.com>
2025-07-23 19:36:56 +0100

    customer and supplier models created

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Customer, Supplier
 
 # Register your models here.
 admin.site.register(Category)
@@ -7,3 +7,9 @@ admin.site.register(Category)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'unit_price', 'total_price']
 admin.site.register(Product, ProductAdmin)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'email', 'phone_number','age']
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Supplier, CustomerAdmin)
